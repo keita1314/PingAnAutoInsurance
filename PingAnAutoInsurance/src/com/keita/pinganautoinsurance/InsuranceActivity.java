ea64a21192c983612f99ed5e885280c54d207732
Keita <dadadpdp@gmail.com>
2013-07-13 12:30:06 +0800

    增加逻辑代码

---
+++ PingAnAutoInsurance/src/com/keita/pinganautoinsurance/InsuranceActivity.java
@@ -0,0 +1,15 @@
+package com.keita.pinganautoinsurance;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InsuranceActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+
+}
--- PingAnAutoInsurance/src/com/keita/pinganautoinsurance/MainActivity.java
+++ PingAnAutoInsurance/src/com/keita/pinganautoinsurance/MainActivity.java
@@ -3,13 +3,26 @@ package com.keita.pinganautoinsurance;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	private Button create_btn = null;
+	private Button view_btn = null;
+	private Button photo_btn = null;
+	private Button record_btn = null;
+	private Button situation_btn = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        create_btn = (Button)findViewById(R.id.create_insurance_policy_btn);
+        view_btn = (Button)findViewById(R.id.all_insurance_policy_btn);
+        photo_btn = (Button)findViewById(R.id.picture_repo_btn);
+        record_btn = (Button)findViewById(R.id.record_repo_btn);
+        situation_btn = (Button)findViewById(R.id.situation_template_btn);
+        
+        
     }
 
     @Override
