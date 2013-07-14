fc6423875ab4217ad8296f076e190ed03be0d530
Keita <dadadpdp@gmail.com>
2013-07-14 10:13:20 +0800

    保单需要输入文本部分初步

--- PingAnAutoInsurance/src/com/keita/pinganautoinsurance/InsuranceActivity.java
+++ PingAnAutoInsurance/src/com/keita/pinganautoinsurance/InsuranceActivity.java
@@ -2,14 +2,27 @@ package com.keita.pinganautoinsurance;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class InsuranceActivity extends Activity {
-
+	private Button continue_btn = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-	}
+		continue_btn =(Button) findViewById(R.id.continue_btn);
+		continue_btn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	
 
+	}
 }
