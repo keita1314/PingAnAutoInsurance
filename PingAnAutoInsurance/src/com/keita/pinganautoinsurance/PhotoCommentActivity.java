7e0240db499d7b1920d89759a255fcc7dce58e97
Keita <dadadpdp@gmail.com>
2013-07-21 14:12:52 +0800

    新增加照片评论页面

---
+++ PingAnAutoInsurance/src/com/keita/pinganautoinsurance/PhotoCommentActivity.java
@@ -0,0 +1,15 @@
+package com.keita.pinganautoinsurance;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhotoCommentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_photo_comment);
+	}
+
+}
