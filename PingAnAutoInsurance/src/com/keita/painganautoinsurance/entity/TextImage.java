4c392b16a9568003afdd95d334c156213cb4ad46
Keita <dadadpdp@gmail.com>
2013-07-21 15:16:40 +0800

    修改为实现Parcelable接口

--- PingAnAutoInsurance/src/com/keita/painganautoinsurance/entity/TextImage.java
+++ PingAnAutoInsurance/src/com/keita/painganautoinsurance/entity/TextImage.java
@@ -1,25 +1,63 @@
 package com.keita.painganautoinsurance.entity;
 
-import java.io.Serializable;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-public class TextImage implements Serializable {
-	//照片评论
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TextImage implements Parcelable {
+	// 照片评论
 	private String text;
-	//照片
+	// 照片
 	private Bitmap image;
+
 	public String getText() {
 		return text;
 	}
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
 	public Bitmap getImage() {
 		return image;
 	}
+
 	public void setImage(Bitmap image) {
 		this.image = image;
 	}
-	
+
+	public static final Creator<TextImage> CREATOR = new Creator<TextImage>() {
+
+		@Override
+		public TextImage createFromParcel(Parcel source) {
+			// TODO Auto-generated method stub
+			TextImage textImage = new TextImage();
+			textImage.text = source.readString();
+			textImage.image = Bitmap.CREATOR.createFromParcel(source);
+			return textImage;
+		}
+
+		@Override
+		public TextImage[] newArray(int size) {
+			// TODO Auto-generated method stub
+			return new TextImage[size];
+		}
+
+	};
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel pracel, int flags) {
+		// TODO Auto-generated method stub
+		pracel.writeString(text);
+		image.writeToParcel(pracel, 0);
+
+	}
+
 }
