91079e4f3e933717541afc97ac0df209a7891734
Keita <dadadpdp@gmail.com>
2013-07-21 10:43:34 +0800

    增加注解

--- PingAnAutoInsurance/src/com/keita/painganautoinsurance/entity/TextImage.java
+++ PingAnAutoInsurance/src/com/keita/painganautoinsurance/entity/TextImage.java
@@ -3,7 +3,9 @@ package com.keita.painganautoinsurance.entity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 public class TextImage {
+	//照片评论
 	private String text;
+	//照片
 	private Bitmap image;
 	public String getText() {
 		return text;
