0aa633d071de1598f0a2d89cb38476ded256d3fa
Keita <dadadpdp@gmail.com>
2013-07-22 21:28:30 +0800

    完成评论功能

--- PingAnAutoInsurance/src/com/keita/pinganautoinsurance/PhotoCommentActivity.java
+++ PingAnAutoInsurance/src/com/keita/pinganautoinsurance/PhotoCommentActivity.java
@@ -7,18 +7,32 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PhotoCommentActivity extends Activity {
 	private ImageView imageView = null;
 	private TextImage textImage = null;
+	private EditText editText = null;
+	private TextView textView = null;
+	private Button comment_btn = null;
 	private Bitmap bitmap = null;
+	private String comment = null;
+	private Intent resultIntent = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photo_comment);
 		imageView = (ImageView) findViewById(R.id.photo_add_comment);
+		editText = (EditText)findViewById(R.id.edittext_add_comment);
+		textView = (TextView)findViewById(R.id.photo_comment_text);
+		comment_btn = (Button)findViewById(R.id.comment_btn);
 		Intent intent = this.getIntent();
 		textImage = (TextImage) intent.getParcelableExtra("TextImage");
 		String imagePath = null;
@@ -26,8 +40,30 @@ public class PhotoCommentActivity extends Activity {
 			imagePath = textImage.getImagePath();
 		bitmap = BitmapFactory.decodeFile(imagePath);
 		imageView.setImageBitmap(bitmap);
-		
+		textView.setText(textImage.getText());
+		resultIntent = new Intent();
+		comment_btn.setOnClickListener(new OnClickListener(){
 
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				if(editText.getText().toString() != null){
+					textView.setText(editText.getText().toString());
+					//textImage.setText(editText.getText().toString());
+					editText.setText("");
+					comment = (String)textView.getText();
+					resultIntent.putExtra("comment", comment);
+				}
+				
+			}
+			
+		});
+		//默认情况 没有进行评论
+		comment = (String)textView.getText();
+		resultIntent.putExtra("comment", comment);
+		resultIntent.setClass(this, InsurancePhotoActivity.class);
+		setResult(2,resultIntent);
+	
 	}
 	@Override
 	protected void onDestroy() {
