719833b63e37723987529882964cba95e30a66c0
Keita <dadadpdp@gmail.com>
2013-07-24 22:53:38 +0800

    修改intent

--- PingAnAutoInsurance/src/com/keita/pinganautoinsurance/MainActivity.java
+++ PingAnAutoInsurance/src/com/keita/pinganautoinsurance/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Intent intent = new Intent();
-				intent.setClass(MainActivity.this,InsurancePhotoActivity.class);
+				intent.setClass(MainActivity.this,InsuranceLocationActivity.class);
 				startActivity(intent);
 			}
         	
