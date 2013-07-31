a87ab91971aafc75bd168f7368b17923d357a0f6
Keita <dadadpdp@gmail.com>
2013-07-31 15:09:42 +0800

    查看保单页面

---
+++ PingAnAutoInsurance/src/com/keita/pinganautoinsurance/InsuranceListActivity.java
@@ -0,0 +1,174 @@
+package com.keita.pinganautoinsurance;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.keita.painganautoinsurance.entity.InsurancePolicy;
+import com.keita.pinganautoinsurance.database.DBHelper;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class InsuranceListActivity extends Activity {
+	private ListView listView = null;
+	private ListAdapter listAdapter = null;
+	private ArrayList<InsurancePolicy> insurancePolicy_list = null;
+	// 数据库操作
+	DBHelper dbHelper = null;
+	SQLiteDatabase dataBase = null;
+	
+	private int page_start = 0;
+	private int page_end = 5;
+	
+	Bitmap bitmap = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_insurance_list);
+		listView = (ListView) findViewById(R.id.insurance_list);
+		insurancePolicy_list = new ArrayList<InsurancePolicy>();
+		dbHelper = new DBHelper(this);
+		dataBase = dbHelper.getWritableDatabase();
+		//列表要显示图片 避免OOM 所以于数据库中的保单分页显示 每页面只显示5个表单
+		loadListView(page_start,page_end);
+		listAdapter = new ListAdapter(this,insurancePolicy_list);
+		listView.setAdapter(listAdapter);
+		
+	}
+	//加载列表
+	public void loadListView(int start,int end){
+		Cursor cur = dbHelper.queryAll(dataBase, "insurance_policy_table");
+		int i = start;
+		if(cur.moveToPosition(start)){
+			do{
+				insurancePolicy_list.clear();
+				Log.v("load", "loadlistview");
+				InsurancePolicy ip = new InsurancePolicy();
+				ip.setInsurancePhotoId(cur.getString(cur.getColumnIndex("photos_id")));
+				System.out.println("loc"+cur.getString(cur.getColumnIndex("location")));
+				ip.setLocation(cur.getString(cur.getColumnIndex("location")));
+				ip.setDate(cur.getString(cur.getColumnIndex("date")));
+				insurancePolicy_list.add(ip);
+			}while(cur.moveToNext() && i < end);
+		}
+		else
+		Toast.makeText(InsuranceListActivity.this, "没有数据", Toast.LENGTH_SHORT).show();
+	} 
+	// 定义adapter
+	class ListAdapter extends BaseAdapter {
+		private Activity context;
+		private List<InsurancePolicy> list;
+
+		public ListAdapter(Activity context, List<InsurancePolicy> list) {
+			this.context = context;
+			this.list = list;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return list.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return list.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			LayoutInflater inflater = context.getLayoutInflater();
+			View itemView = inflater
+					.inflate(R.layout.insurance_list_item, null);
+			InsurancePolicy currentInsurancePolicy = list.get(position);
+
+			ImageView imageView = (ImageView) itemView
+					.findViewById(R.id.insurance_item_img);
+			TextView textViewLoaction = (TextView) itemView
+					.findViewById(R.id.insurance_item_location);
+			TextView textViewDate = (TextView) itemView
+					.findViewById(R.id.insurance_item_date);
+			// 设置item的内容
+			bitmap = getImage(currentInsurancePolicy.getInsurancePhotoId());
+			if(bitmap != null)
+				imageView.setImageBitmap(bitmap);
+			textViewLoaction.setText(currentInsurancePolicy.getLocation());
+			textViewDate.setText(currentInsurancePolicy.getDate());
+			return itemView;
+		}
+
+	}
+	//根据保单Id获得第一张图作为缩略图
+	public Bitmap getImage(String photoId){
+		
+		Cursor cur = dbHelper.query(dataBase, "insurance_photo_table", new String[]{"img1_id"},"id=?",new String[]{photoId},null,null,null);
+		String img_id = null;
+		if(cur.moveToFirst())
+			img_id = cur.getString(cur.getColumnIndex("img1_id"));
+		cur.close();
+		//根据Id查询得到图片
+		
+		cur = dbHelper.query(dataBase, "text_image_table", new String[]{"img_path"},"id=?",new String[]{img_id},null,null,null);
+		String img_path =null;
+		if(cur.moveToFirst())
+			img_path= cur.getString(cur.getColumnIndex("img_path"));
+		
+		//根据路径得到图片
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 8;
+		InputStream is = null;
+		Bitmap source = null;
+		;
+		
+		try {
+			is = new FileInputStream(img_path);
+			source = BitmapFactory.decodeStream(is, null, options);
+			bitmap = ThumbnailUtils.extractThumbnail(source, 100,
+					100);
+			if(source != null && !source.isRecycled())
+			source.recycle();
+			is.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return bitmap;
+		
+	}
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		dataBase.close();
+		dbHelper.close();
+		if(bitmap != null &!bitmap.isRecycled())
+			bitmap.recycle();
+	}
+	
+}
--- PingAnAutoInsurance/src/com/keita/pinganautoinsurance/database/DBHelper.java
+++ PingAnAutoInsurance/src/com/keita/pinganautoinsurance/database/DBHelper.java
@@ -1,5 +1,127 @@
 package com.keita.pinganautoinsurance.database;
 
-public class DBHelper {
-	
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+	@Override
+	public void onOpen(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		super.onOpen(db);
+	}
+
+	// 数据库名称
+	private static final String DATABASE_NAME = "PingAnDB";
+	// 数据库版本
+	private static final int DATABSE_VERSION = 1;
+	// 表名
+	// 保单表
+	private static final String INSURANCE_POLICY_TABLE = "insurance_policy_table";
+	// 带评论的照片表
+	private static final String TEXT_IMAGE_TABLE = "text_image_table";
+	// 保单对应图片表
+	private static final String INSURANCE_PHOTO_TABLE = "insurance_photo_table";
+	// 保单对应的文本表
+	private static final String INSURANCE_TEXT_TABLE = "insruace_text_table";
+
+	// 创建表的sql语句
+	private static final String TEXT_IMAGE_TABLE_SQL = "CREATE TABLE "
+			+ TEXT_IMAGE_TABLE
+			+ "(id integer primary key,img_path text,img_text text,img_date text);";
+
+	private static final String INSURANCE_PHOTO_TABLE_SQL = "CREATE TABLE "
+			+ INSURANCE_PHOTO_TABLE
+			+ "(id integer primary key autoincrement,img1_id integer,img2_id integer ,"
+			+ "img3_id integer ,img4_id integer ,img5_id integer ,img6_id integer,"
+			+ "foreign key(img1_id) references text_image_table(id),foreign key(img2_id) references text_image_table(id),"
+			+ "foreign key(img3_id) references text_image_table(id) ,foreign key(img4_id) references text_image_table(id)"
+			+ "foreign key(img5_id) references text_image_table(id),foreign key(img6_id) references text_image_table(id));";
+
+	private static final String INSURANCE_TEXT_SQL = "CREATE TABLE "
+			+ INSURANCE_TEXT_TABLE
+			+ "(text_id integer primary key autoincrement,case_no text,case_owner text,case_driver text,realtion text,"
+			+ "case_owner_phone text,case_driver_phone text,case_drvier_lience text,case_car_no text,case_car_type,vin text,"
+			+ "case_third_car_no text,case_third_car_type,hurt_num integer,dead_num integer,case_reason text,accident_reason text,"
+			+ "accident_detail text);";
+	private static final String INSURANCE_POLICY_SQL = "CREATE TABLE "
+			+ INSURANCE_POLICY_TABLE
+			+ "(policy_id integer primary key autoincrement,location text,record_path text,date text,photos_id integer,text_id integer,"
+			+ "foreign key(photos_id) references insurance_photo_table(id),foreign key(text_id) references insurance_text_table(text_id));";
+
+	public DBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABSE_VERSION);
+		// TODO Auto-generated constructor stub
+		Log.v("db", "const");
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		Log.v("db", "created");
+		db.execSQL(TEXT_IMAGE_TABLE_SQL);
+		db.execSQL(INSURANCE_PHOTO_TABLE_SQL);
+		db.execSQL(INSURANCE_TEXT_SQL);
+		db.execSQL(INSURANCE_POLICY_SQL);
+	}
+
+	// 插入数据 根据表
+	public void insertData(SQLiteDatabase db, ContentValues cv, String tableName) {
+		// 单个照片表
+		if (tableName.equals(TEXT_IMAGE_TABLE)) {
+			db.insert(tableName, null, cv);
+		}
+		// 保单照片集表
+		if (tableName.equals(INSURANCE_PHOTO_TABLE)) {
+			db.insert(tableName, "id", cv);
+		}
+		if (tableName.equals(INSURANCE_TEXT_TABLE)) {
+			db.insert(tableName, "text_id", cv);
+		}
+		if (tableName.equals(INSURANCE_POLICY_TABLE)) {
+			db.insert(tableName, "policy_id", cv);
+		}
+	}
+
+	public void deleteData(SQLiteDatabase db, String whereClause,
+			String[] whereArgs, String tableName) {
+		if (tableName.equals(TEXT_IMAGE_TABLE)) {
+			db.delete(tableName, whereClause, whereArgs);
+		}
+	}
+
+	public void update() {
+
+	};
+
+	// 按列查询
+	public Cursor queryByCol(SQLiteDatabase db, String tableName,
+			String[] columns) {
+		return db.query(tableName, columns, null, null, null, null, null);
+	}
+
+	// 查询所有
+	public Cursor queryAll(SQLiteDatabase db, String tableName) {
+		return db.query(tableName, null, null, null, null, null, null);
+	}
+
+	// 常规查询
+	public Cursor query(SQLiteDatabase db, String tableName, String[] columns,
+			String selection, String[] selectionArgs, String groupBy,
+			String having, String orderBy) {
+		return db.query(tableName, columns, selection, selectionArgs, groupBy,
+				having, orderBy);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		Log.v("DB", "update");
+	}
+
 }
